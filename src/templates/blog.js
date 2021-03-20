2d391117a1aead1d6147d3776e1b7739eb113855
dehanz13 <hansel.danniel@gmail.com>
2021-03-19 22:53:36 -0500

    tried to get the body rendered

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { graphql } from 'gatsby';
-import {documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { graphql, Link, useStaticQuery } from 'gatsby';
+//import {documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { renderRichText } from 'gatsby-source-contentful/rich-text';
 
 import Layout from '../components/layout'
 
-// export const query = graphql(`
+// export const query = graphql`
 // 	query($slug: String!) {
 // 		markdownRemark(
 // 			fields: { 
@@ -20,19 +21,66 @@ import Layout from '../components/layout'
 // 			html
 // 		}
 // 	}
-// `)
+// `
 
-export const query = graphql(`
+export const query = graphql`
 	query($slug: String!) {
 		contentfulBlogPost (slug: {eq: $slug}) {
 			title
 			publishedDate(formatString: "MMMM Do, YYYY")
 		}
 	}
-`)
+`
+
+// export const query = graphql`
+// 	query($slug: String!) {
+// 		AllContentfulBlogPost {
+// 			edges {
+// 				node {
+// 					bodyRichText {
+// 						raw
+// 						references {
+// 							... on ContentfulAsset {
+// 								contentful_id{
+// 									fixed(width: 1600 ) {
+// 										width
+// 										height
+// 										src
+// 										srcSet
+// 									}
+// 								}
+// 								... on ContentfulBlogPost {(slug: {eq: $slug})} {
+// 									title
+// 									publishedDate(formatString: "MMMM Do, YYYY")
+// 								}
+// 							}
+// 						}
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// `
 
 const Blog = (props) => {
-	
+	// const options = {
+	// 	"embedded-asset-block":node => {
+	// 		return(
+	// 			<>
+	// 				<pre>
+	// 					<code>
+	// 						{JSON.stringify(node, null, 2)}
+	// 					</code>
+	// 				</pre>
+	// 				<img
+	// 					src={node.data.target.fixed.src}
+	// 					alt={node.data.target.title}
+	// 				/>
+	// 			</>
+	// 		)
+	// 	},
+	// }
+
 	return(
 		<Layout>
 			{/* <h1>{props.data.markdownRemark.frontmatter.title}</h1>
@@ -41,7 +89,10 @@ const Blog = (props) => {
 			</div> */}
 			<h1>{props.data.contentfulBlogPost.title}</h1>
 			<p>{props.data.contentfulBlogPost.publishedDate}</p>
+			{/* {body && renderRichText(data.contentfulBlogPost.body, options)} */}
+			
 		</Layout>
 	)
 }
+
 export default Blog;
